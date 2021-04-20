720ca46f1c3ba32bfbe780b8cd7224565acc2e64
MuhammdSheraz <mshazy999@gmail.com>
2021-04-20 09:19:15 +0500

    likes and commit count added

--- Graphql/Resolvers/Index.js
+++ Graphql/Resolvers/Index.js
@@ -4,6 +4,15 @@ const CommentResolver = require("./CommentResolver");
 const LikeResolver = require("./LikeResolver");
 
 module.exports = {
+  // For Like and Comment Count
+  Post: {
+    likesCount(parent) {
+      return parent.likes.length;
+    },
+    commentsCount(parent) {
+      return parent.comments.length;
+    },
+  },
   Query: {
     ...PostResolver.Querry,
   },
